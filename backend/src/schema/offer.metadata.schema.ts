cc546fda34d9e02129ddaf2dccc052b51f4505a6
kunalmkv <kunalm4.kvtagore@gmail.com>
2025-09-27 20:02:14 +0530

    joi event id

--- backend/src/schema/offer.metadata.schema.ts
+++ backend/src/schema/offer.metadata.schema.ts
@@ -1,8 +1,12 @@
 import joi from 'joi';
 
 export default joi.object({
+    eventId: joi.number().positive().optional(),
+    type: joi.string().valid('OFFER_TO_BUY', 'OFFER_TO_SELL').required(),
+    sellerAddress: joi.string().required(),
+    buyerAddress: joi.string().allow(null).optional(),
     quantity: joi.number().positive().required(),
-    seatNumbers: joi.array().items(joi.required()).optional(),
+    seatNumbers: joi.array().items(joi.string()).optional(),
     seatType: joi.string().required(),
     isPhysicalTicketNeededToAttend: joi.boolean().required(),
 });
