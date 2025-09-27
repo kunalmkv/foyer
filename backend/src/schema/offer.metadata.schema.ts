34f5ee4ab53f815c2ae0756e24d7aa1e982f3338
ciphernova-ship-it <mohammad.numan@triremetrading.com>
2025-09-27 22:54:27 +0530

    some fixes in offer controller

--- backend/src/schema/offer.metadata.schema.ts
+++ backend/src/schema/offer.metadata.schema.ts
@@ -1,8 +1,6 @@
 import joi from 'joi';
 
 export default joi.object({
-    sellerAddress: joi.string().required(),
-    buyerAddress: joi.string().allow(null).optional(),
     quantity: joi.number().positive().required(),
     seatNumbers: joi.array().items(joi.string()).optional(),
     seatType: joi.string().required(),
