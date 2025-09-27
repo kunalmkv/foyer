bac7d8cbe44f75db8b9bd2c733ec921a07e40711
vish-han <vshlchauhab@gmail.com>
2025-09-28 02:43:08 +0530

    config updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        turbopack: true,
-    },
+
     typescript: {
         // Skip type checking during build
         ignoreBuildErrors: true,
